f2beb20076deee1d9199e6374380b7812dc11344
codewithdaniyal <167603648+codewithdaniyal@users.noreply.github.com>
2024-06-04 09:42:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+var qoute = "A person who never made a mistake never tried anything new";
+var peronName = "Albert eintstein";
+console.log("".concat(peronName, " once said, ").concat(qoute));
---
+++ app.ts
@@ -0,0 +1,4 @@
+let qoute:string = "A person who never made a mistake never tried anything new"
+let author:string = "Albert eintstein"
+
+console.log(`${author} once said, ${qoute}`)
